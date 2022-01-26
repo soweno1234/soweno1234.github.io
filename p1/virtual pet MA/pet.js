f11a78be066265c0772bb1850c1563c3367d9f3e
Soweno1234 <sghorai@eastsideprep.org>
2022-01-25 22:10:40 -0800

    main

---
+++ p1/virtual pet MA/pet.js
@@ -0,0 +1,12 @@
+let interest = 0;
+let belly = 0;
+let happiness = 0;
+
+function feedPet() {
+    //this function will make the pet less hungry by giving it food. as the belly gets more emtpy, so will the happiness
+}
+
+function playPet() {
+    /*this function will make the player play games with the pet, and it will increase the pet's interest level. 
+    A combination of this and the hunger level both influence the overall happiness.*/
+}
