01bb2591504a0fe983cccc52ce59975a84dee69a
Soweno1234 <sghorai@eastsideprep.org>
2022-01-26 14:59:53 -0800

    main

---
+++ p1/donald dog virtual pet/js/pet.js
@@ -0,0 +1,14 @@
+let interest = 0;
+let belly = 0;
+let happiness = 0;
+
+function feedPet() {
+    console.log("this will increase the belly variable");
+    //this function will make the pet less hungry by giving it food. as the belly gets more emtpy, so will the happiness
+}
+
+function playPet() {
+    console.log("this will increase the interest level");
+    /*this function will make the player play games with the pet, and it will increase the pet's interest level. 
+    A combination of this and the hunger level both influence the overall happiness.*/
+}
