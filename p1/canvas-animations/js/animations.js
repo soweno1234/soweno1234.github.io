1836a264c336ae450e2d246e543fae1c2e5ee4d8
Soweno1234 <sghorai@eastsideprep.org>
2022-02-14 09:13:13 -0800

    main

--- p1/canvas-animations/js/animations.js
+++ p1/canvas-animations/js/animations.js
@@ -2,58 +2,136 @@
 let myCanvas = document.getElementById("my-canvas");
 let ctx = myCanvas.getContext("2d");
 
-// object position
-/*
-let xPosition = 0;
+//get paragraph items
+let keyDownOutput = document.getElementById("keydown-output");
+let keyUpOutput = document.getElementById("keyup-output");
 
-function moveHorizontal() {
-    //clear screen
+
+//player position and movement
+let playerX = 250;
+let playerY = 250;
+let playerXDir = 0;
+let playerYDir = 0;
+let playerSpeed = 2;
+const PADDLE_WIDTH = 100;
+const PADDLE_HEIGHT = 20;
+
+//ball position and movement
+let ballX = 100;
+let ballY = 100;
+let ballXDir = 8;
+let ballYDir = 8;
+const BALL_RADIUS = 20;
+
+
+
+function drawPlayer() {
+    ctx.fillRect(playerX, playerY, PADDLE_WIDTH, PADDLE_HEIGHT);
+}
+
+function movePlayer() {
+    playerX += playerSpeed * playerXDir;
+    playerY += playerSpeed * playerYDir;
+
+    //edge check
+    if (playerX < 0) {
+        playerX = 0;
+    } else if (playerX > 500 - PADDLE_WIDTH) {
+        playerX = 500 - PADDLE_WIDTH;
+    } else if (playerY < 0) {
+        playerY = 0;
+    } else if (playerY > 500 - PADDLE_HEIGHT) {
+        playerY = 500 - PADDLE_HEIGHT
+    }
+}
+
+function drawBall() {
+    //draw a filed circle at ballX and ballY
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, BALL_RADIUS, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+function moveBall() {
+
+    ballX += ballXDir;
+    ballY += ballYDir;
+
+}
+
+function checkBallCollision() {
+    //check vertical wall
+    if ((ballY > 500 - BALL_RADIUS) || (ballY < 0 + BALL_RADIUS)) {
+        ballYDir = ballYDir * -1
+        ballYDir = ballYDir * 1.01
+
+    }
+
+    if ((ballX > 500 - BALL_RADIUS) || (ballX < 0 + BALL_RADIUS)) {
+        ballXDir = ballXDir * -1;
+        ballXDir = ballXDir * 1.01;
+    }
+
+    //check for paddle collision
+    if (ballX + BALL_RADIUS >= playerX &&
+        ballX - BALL_RADIUS <= playerX + PADDLE_WIDTH &&
+        ballY + BALL_RADIUS >= playerY &&
+        ballY - BALL_RADIUS <= playerY + PADDLE_HEIGHT)
+        ballYDir = ballYDir * -1;
+}
+
+
+function refreshUI() {
     ctx.clearRect(0, 0, 500, 500);
+    movePlayer();
+    drawPlayer();
+    checkBallCollision();
+    drawBall();
+    moveBall();
+}
 
-    //draw rectangle at current position
-    ctx.fillRect(xPosition, 0, 20, 20);
+// when key is pressed
+function keyPressed(event) {
+    //get the actual key pressed
+    let key = event.keyCode;
+    keyDownOutput.innerHTML = "key down code: " + key;
 
-    //move the x position over by x pixels
-    xPosition += 1;
-
-
-    //did I hit the wall? if so wrap around
-    if (xPosition >= 500) {
-        xPosition = 0;
+    //move player
+    if (key === 37) {
+        playerXDir = -6;
+    } else if (key === 39) {
+        playerXDir = 6;
+    } else if (key === 38) {
+        playerYDir = -6;
+    } else if (key === 40) {
+        playerYDir = 6;
     }
 }
 
 
-setInterval(moveHorizontal, .5);
+function keyReleased(event) {
+    //get the actual key released
+    let keyRelease = event.keyCode;
+    keyUpOutput.innerHTML = "key up code: " + keyRelease;
+
+    if (keyRelease === 37) {
+        playerXDir = 0;
+    } else if (keyRelease === 39) {
+        playerXDir = 0;
+    }
+
+    if (keyRelease === 40) {
+        playerYDir = 0;
+    } else if (keyRelease === 38) {
+        playerYDir = 0;
+    }
+}
 
 
-//rect moving vertically then wrapping around
-// object position
+let xPosition = 0;
 let yPosition = 0;
-
-
-function moveVertical() {
-    //clear screen
-    ctx.clearRect(0, 0, 500, 500);
-
-    //draw rectangle at current position
-    ctx.fillRect(0, yPosition, 20, 20);
-
-    //move the y position over by y pixels
-    yPosition += 1;
-
-
-    //did I hit the wall? if so wrap around
-    if (yPosition >= 500) {
-        yPosition = 0;
-    }
-}
-
-setInterval(moveVertical, .5);
-*/
-
-let xPosition = 0;
 let xSpeed = 1;
+let ySpeed = 1;
 
 
 
@@ -81,4 +159,6 @@ function moveHorizontal() {
 
 
 
-setInterval(moveHorizontal, 5)
+
+// setInterval(moveHorizontal, 5);
+setInterval(refreshUI, 30);
