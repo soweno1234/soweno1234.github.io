3065011f315c418d3c6c7ab9c409853071c11b50
Soweno1234 <sghorai@eastsideprep.org>
2022-11-13 13:02:43 -0800

    w order jsavscript

--- final/js/order.js
+++ final/js/order.js
@@ -1,18 +1,9 @@
-let fname = "";
+let fname = document.getElementById("fname").value;
+let lname = document.getElementById("lname").value;
+let age = document.getElementById("age").value;
+let address = document.getElementById("address").value;
 
-//function for greet
-function greet() {
-    //grab the greet paragraph
-    let greetParagraph = document.getElementById("greet");
 
-    //get values from form
-    fname = document.getElementById("fname").value;
-    //let lname = document.getElementById("lname").value;
-    let age = document.getElementById("age").value;
-
-    //show results!
-    greetParagraph.innerHTML = "Greetings " + age + " year-old " + fname + "!";
-}
 
 
 
@@ -27,6 +18,113 @@ function truthAndLie() {
     let address = document.getElementById("address").value;
 
 
-    truthAnswer.innerHTML = "Hi " + age + " year old " + fname + " " + lname + ". Your food will be delivered to " + address + ". Place your order below."
+    truthAnswer.innerHTML = "Hi " + age + " year old " + fname + " " + lname + ". Your food will be delivered to " + address + ". Place your order below. Pick one dish from each section."
+
+}
+
+// function to trivia game
+function truthAndLie1() {
+    //decide what to print
+
+    // get paragraph
+    let truthAnswer1 = document.getElementById("truth-answer1");
+    let finalOrder = "Thanks for your order " + fname + "! <br> Your appetizer: ";
+
+    //get the checked values from the form
+    //appetizers
+    let sateAyamSelected = document.getElementById("SateAyam").checked;
+    let pangsitGorengSelected = document.getElementById("PangsitGoreng").checked;
+    let tempeMendoanSelected = document.getElementById("TempeMendoan").checked;
+    let risolesSelected = document.getElementById("Risoles").checked;
+    let siomaySelected = document.getElementById("SiomayCampurBandung").checked;
+    let springRollSelected = document.getElementById("FriedSpringRoll").checked;
+    let chickenSelected = document.getElementById("ChickenandFishTofu").checked;
+
+    //meat and seafood
+    let baladoSelected = document.getElementById("AyamGorengBalado").checked;
+    let cambeiSelected = document.getElementById("AyamCabeiJo").checked;
+    let ikanSelected = document.getElementById("IkanBakarKecap").checked;
+    let empekSelected = document.getElementById("EmpekEmpekPalembang").checked;
+    let gorengSelected = document.getElementById("IkanGorengLalapan").checked;
+    let miniSelected = document.getElementById("MiniRijsttafelAyamMbokBerek").checked;
+    let rijstaffelSelected = document.getElementById("MiniRijsttafelRendangBeef").checked;
+    let rendangSelected = document.getElementById("RendangSapi").checked;
+    let mbokSelected = document.getElementById("AyamGorengMbokBerek").checked;
+
+    //rice and noodle
+    let bakmiSelected = document.getElementById("BakmiGorengJawa").checked;
+    let bihunSelected = document.getElementById("BihunBakso").checked;
+    let nasiSelected = document.getElementById("NasiKuningKomplit").checked;
+    let indonesiaSelected = document.getElementById("NasiGorengIndonesia").checked;
+
+    //appetizer
+    if (sateAyamSelected) {
+        finalOrder = finalOrder + "Sate Ayam <br> Your meat/seafood dish:";
+    }
+    if (pangsitGorengSelected) {
+        finalOrder = finalOrder + "Pangsit Goreng <br> Your meat/seafood dish:";
+    }
+    if (tempeMendoanSelected) {
+        finalOrder = finalOrder + "Tempe Mendoan <br> Your meat/seafood dish:";
+    }
+    if (risolesSelected) {
+        finalOrder = finalOrder + "Risoles <br> Your meat/seafood dish:";
+    }
+    if (siomaySelected) {
+        finalOrder = finalOrder + "Siomay Campur Bandung <br> Your meat/seafood dish:";
+    }
+    if (springRollSelected) {
+        finalOrder = finalOrder + "Fried Spring Roll <br> Your meat/seafood dish:";
+    }
+    if (chickenSelected) {
+        finalOrder = finalOrder + "Chicken and Fish Tofu <br> Your meat/seafood dish:";
+    }
+
+    //meat and seafood
+    if (baladoSelected) {
+        finalOrder = finalOrder + " Ayam Goreng Balado <br> Your rice/noodle dish:";
+    }
+    if (cambeiSelected) {
+        finalOrder = finalOrder + " Ayam Cabei Jo <br> Your rice/noodle dish:";
+    }
+    if (ikanSelected) {
+        finalOrder = finalOrder + " Ikan Bakar Kecap <br> Your rice/noodle dish:";
+    }
+    if (empekSelected) {
+        finalOrder = finalOrder + " Empek-Empek Palembang <br> Your rice/noodle dish:";
+    }
+    if (gorengSelected) {
+        finalOrder = finalOrder + " Ikan Goreng Lalapan <br> Your rice/noodle dish:";
+    }
+    if (miniSelected) {
+        finalOrder = finalOrder + " Mini Rijsttafel Ayam Mbok Berek <br> Your rice/noodle dish:";
+    }
+    if (rijstaffelSelected) {
+        finalOrder = finalOrder + " Mini Rijstaffel Rendang Beef <br> Your rice/noodle dish:";
+    }
+    if (rendangSelected) {
+        finalOrder = finalOrder + " Rendang Sapi <br> Your rice/noodle dish:";
+    }
+    if (mbokSelected) {
+        finalOrder = finalOrder + " Ayam Goreng Mbok Berek <br> Your rice/noodle dish:";
+    }
+
+
+    //rice and noodle
+    if (bakmiSelected) {
+        finalOrder = finalOrder + " Bakmi Goreng Jawa <br> Have a nice day!";
+    }
+    if (bihunSelected) {
+        finalOrder = finalOrder + " Bihun Bakso <br> Have a nice day!";
+    }
+    if (nasiSelected) {
+        finalOrder = finalOrder + " Nasi Kuning Komplit <br> Have a nice day!";
+    }
+    if (indonesiaSelected) {
+        finalOrder = finalOrder + " Nasi Goreng Indonesia <br> Have a nice day!";
+    }
+
+    //print
+    truthAnswer1.innerHTML = finalOrder;
 
 }
--- form-demo/js/game.js
+++ form-demo/js/game.js
@@ -15,6 +15,26 @@ function greet() {
 }
 
 
+// function to trivia game
+function trivia() {
+    // trivia answer
+    let triviaAnswer = document.getElementById("trivia-answer");
+    // get the checked values from the form
+    let chocolateSelected = document.getElementById("chocolate").checked;
+    let tunaSelected = document.getElementById("tuna").checked;
+    let honeySelected = document.getElementById("honey").checked;
+
+    // decide what to print
+    if (chocolateSelected) {
+        triviaAnswer.innerHTML = fname + ", you are wrong. Sweet tooth, eh?";
+    } else if (tunaSelected) {
+        triviaAnswer.innerHTML = fname + ", you are wrong. Rotten tuna, yuck!";
+    } else if (honeySelected) {
+        triviaAnswer.innerHTML = fname + ", you are correct!";
+    }
+
+}
+
 
 // function to trivia game
 function truthAndLie() {
